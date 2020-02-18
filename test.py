3e578e42dc2b6d49919c920000fad025ee3dd995
Dorvin <ladig1017@gmail.com>
2020-02-18 23:58:10 +0900

    multiple

--- test.py
+++ test.py
@@ -7,9 +7,13 @@ options.add_argument("disable-gpu")
 
 driver = webdriver.Chrome('./chromedriver', chrome_options=options)
 driver.implicitly_wait(3)
-driver.get('http://13.124.52.157/screensharingtest.html')
 
-driver.find_element_by_id('start').click()
-driver.find_element_by_id('roomid').click()
-driver.find_element_by_id('roomid').send_keys('2499806332950512')
-driver.find_element_by_id('join').click()
+for i in range(2,12):
+	driver.get('http://13.124.52.157/screensharingtest.html')
+	driver.find_element_by_id('start').click()
+	driver.find_element_by_id('roomid').click()
+	driver.find_element_by_id('roomid').send_keys('368101203117938')
+	driver.find_element_by_id('join').click()
+	driver.execute_script("window.open('about:blank'," + " '" + "tab" + str(i) + "');")
+	driver.switch_to.window("tab"+str(i))
+print('test is completed successfully')
